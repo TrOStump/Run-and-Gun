7649439a814bcbc95aa7848a909f61b261d68e5e
Oates <G00260698@stu.raritanval.edu>
2017-12-04 16:20:44 -0500

    Update 1

---
+++ Run and gun/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S; // a FollowCam Singleton
+
+    //fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool __________________________;
+
+    //fields set Dynamically
+    public GameObject poi; // point of interest
+    public float camZ; // the desired Z pos of the camera
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+        // limit the x and y to minumum values
+    }
+    void FixedUpdate()
+    {
+        Vector3 destination;
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            //get the position poi
+            destination = poi.transform.position;
+            //if poi is a projectile,  check to see if it's at rest
+
+
+            // limit the x and y to minumum values
+            destination.y = Mathf.Max(minXY.y, destination.y);
+            //Interpolate from the current Camera position towards destination
+            destination = Vector3.Lerp(transform.position, destination, easing);
+            //retain a destination.z of camZ
+            destination.z = camZ;
+            //set the camera to the destination
+            transform.position = destination;
+            //set the orthographicsize of the camera t keep the ground in view
+            this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+        }
+
+
+    }
+}
+
---
+++ Run and gun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+   
+}
